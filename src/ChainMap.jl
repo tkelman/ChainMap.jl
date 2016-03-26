9bd1b445d9855e27a8808b53b0e193b5544d8dac
Brandon Taylor <brandon.taylor221@gmail.com>
2016-03-26 14:18:17 -0400

    Reverted to let blocks, switched documentation to macro versions.

--- src/ChainMap.jl
+++ src/ChainMap.jl
@@ -1,21 +1,49 @@
 module ChainMap
-import MacroTools, Base.replace
+import MacroTools
 export chain, @chain, lambda, @lambda, chain_map, @chain_map, map_all
 
-function replace_recur(ex, pat, r)
-  if ex == pat
-    r
-  elseif typeof(ex) <: Expr
-    ex.head = ex.head == pat ? r : ex.head
-    ex.args = map(x -> replace_recur(x, pat, r), ex.args)
-    ex
-  else
-    ex
-  end
-end
-
 """
-Non-standard evaluation version of `chain`.
+    @chain x
+
+Separate single blocks out into lines and recur, return single non-blocks.
+
+    @chain begin
+             1
+             +(1)
+           end
+
+is the same as `@chain 1 +(1)`
+
+    @chain x ex
+
+`@chain` always substitutes `x` into `\_` in `ex`. `@chain 1 -(2, \_)` returns `-(2, 1)`
+
+In addition, insertion of `x` to the first argument of `ex` is default. `@chain 1 +(1)` returns `+(1, 1)`
+
+Insertion is overridden in three ways:
+
+- If bare `\_` is an argument to `ex`.
+See the first example
+
+- If `ex` is a block.
+
+    @chain 1 begin
+               b = 2
+               -(b, \_)
+             end
+
+will translate to
+
+    begin
+      b = 2
+      -(b, 1)
+    end
+
+- If `ex` is a lambda. `@chain 1, x -> x + \_` will translate to `x -> x + 1`
+
+    @chain x exs...
+
+Reduce `@chain` over `(x, exs...)`. `@chain 1 +1 +1` is the same as +(+(1, 1), 1)
 """
 macro chain(exs...)
   esc(chain(exs...))
@@ -23,67 +51,51 @@ end
 
 """
     chain(x)
+    chain(x, ex)
+    chain(x, exs...)
 
-Separate single blocks out into lines and recur, return single non-block expressions.
+Standard evaluation version of `@chain`.
 """
 function chain(x)
   MacroTools.isexpr(x, :block) ? chain(MacroTools.rmlines(x).args...) : x
 end
 
-"""
-    chain(x, ex)
-
-`chain` always substitutes `x` into `\_` in `ex`. `chain(:a, :( -(b, \_) ) )` returns `:( -(b, a) )`
-
-In addition, insertion of `x` to the first argument of `ex` is default. `chain(:a, :( +(b) ) )` returns `:( +(a, b) )`
-
-Insertion is overridden in three ways:
-
-1) If bare `\_` is an argument to `ex`.
-See the first example
-
-2) If `ex` is a block. `chain(:a, quote b = 1; -(b, \_) end)` will translate to `:( quote b = 1; -(b, a) end )`
-
-3) If `ex` is a lambda. `chain(:a, :(x -> x + \_) )` will translate to `:(x -> x + a)`
-"""
 function chain(x, ex)
   # bare function calls
   if MacroTools.isexpr(ex, Symbol)
     Expr(:call, ex, x)
   # substitution only
   elseif (:_ in ex.args) | MacroTools.isexpr(ex,  :->, :block)
-    replace_recur(ex, :_, x)
+    :(let _ = $x
+        $ex
+      end)
   # insertion and substitution for non-functions
   elseif MacroTools.isexpr(ex, :vect, :tuple)
     ex_insert = Expr(ex.head, x, ex.args...)
-    replace_recur(ex_insert, :_, x)
+    :(let _ = $x
+        $ex_insert
+      end)
   # insertion and substitution for function calls
   elseif MacroTools.isexpr(ex, :call, :macrocall)
     ex_insert = Expr(ex.head, ex.args[1],
                      x, ex.args[2:end]...)
-    replace_recur(ex_insert, :_, x)
+    :(let _ = $x
+        $ex_insert
+      end)
   else
     error("Unsupported expression $ex")
   end
 end
 
-"""
-    chain(x, exs...)
-
-Reduce `chain` over `(x, exs...)`
-"""
 function chain(x, exs...)
   reduce(chain, x, exs)
 end
 
 """
-    lambda(ex...)
+    lambda(ex)
+    lambda(exs...)
 
-Will chain together `ex...` expressions using `chain` rules above.
-Then, an anonymous function is constructed, with \_ as an input varible.
-The input variable may or may not be inserted as the first argument of the first expression.
-`lambda(:(-(1, \_)), :(+(2)))` will return `:(\_ -> +(-(1, \_), 2))` and
-`lambda(:(-(1)), :(+(2)))` will return `:(\_ -> +(-(\_, 1), 2))`
+Standard evaluation version of `@lambda`.
 """
 function lambda(ex)
   x = gensym()
@@ -101,7 +113,13 @@ function lambda(exs...)
 end
 
 """
-Non-standard evaluation version of `lambda`
+    @lambda ex...
+
+Will chain together `ex...` expressions using `chain` rules above.
+Then, an anonymous function is constructed, with \_ as an input varible.
+The input variable may or may not be inserted as the first argument of the first expression.
+`@lambda -(2, \_)` will return `\_ -> -(2, \_)` and
+`@lambda +(1)` will return `\_ -> +(\_, 1)`
 """
 macro lambda(exs...)
   esc(lambda(exs...))
@@ -110,15 +128,17 @@ end
 """
     chain_map(x, exs...)
 
-will build the code for an anoymous function by chaining together `exs...` using `lambda`,
-then build a map expression for mapping that function over `x`.
+Standard evaluation version of `@chain_map`.
 """
 chain_map = function(x, exs...)
   Expr(:call, :map, ChainMap.lambda(exs...), x)
 end
 
 """
-Non-standard evaluation version of `chain_map`.
+    @chain_map x exs...
+
+will build an anoymous function by chaining together `exs...` using `@lambda`,
+then map that function over `x`.
 """
 macro chain_map(exs...)
   esc(chain_map(exs...))
