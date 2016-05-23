751a0296dd182aeb9bb7110e945e2152d4c1a8c8
Brandon Taylor <brandon.taylor221@gmail.com>
2016-05-22 20:37:34 -0400

    Added more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -27,6 +27,10 @@ end
 
 Test.@test test_function(1) == 4
 
+test_chain_function = @l -(2, _) +(1)
+
+Test.@test test_chain_function(1) == 2
+
 test_map = @c begin
   1
   x -> x^2 + _
@@ -73,3 +77,18 @@ readme = @o @c begin
 end
 
 Test.@test readme == [2, 4]
+
+a = ( [1, 2], [3, 4] )
+dots_test = @o +( ~(a...) )
+Test.@test dots_test == [4, 6]
+
+b = ( [5, 6], [7, 8] )
+
+errrror =
+  try
+    over( :( +( ~(a...), ~(b...) ) ) )
+  catch x
+    x
+  end
+
+Test.@test errrror.msg == "Cannot map over more than one splatted argument"
