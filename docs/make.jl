af7f758c2604a089bcf2a3095cf3d0e433903fa3
Brandon Taylor <brandon.taylor221@gmail.com>
2016-07-27 02:47:24 -0400

    more doc changes

--- docs/make.jl
+++ docs/make.jl
@@ -3,5 +3,6 @@ using Documenter, ChainMap
 makedocs()
 
 deploydocs(
-    repo = "github.com/bramtayl/ChainMap.jl.git"
+    repo = "github.com/bramtayl/ChainMap.jl.git",
+    deps = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math")
 )
