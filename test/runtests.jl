be12457285bd70eac87889cba20207ea60b335b0
Brandon Taylor <brandon.taylor221@gmail.com>
2016-08-12 17:42:54 -0400

    bugfixes

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,16 +19,16 @@ a = collect_arguments(1, 2, a = 3, b = 4)
 test_function(a, b) = vcat(a, b)
 
 @test (@chain begin
-                  collect_call(test_function, [1, 2], [3])
+                  collect_call(test_function, [1, 2], [3, 4])
                   run(broadcast)
               end) ==
-      [ [1, 3], [2, 3] ]
+      [ [1, 3], [2, 4] ]
 Test.@test (@chain begin
-                 collect_call(+, [1, 2], [3, 4])
+                 collect_call(vcat, [1, 2], [3, 4])
                  collect_arguments(map)
                  run
              end) ==
-      [4, 6]
+      [ [1, 3], [2, 4] ]
 test_function(a, b; c = 4) = a - b + c
 
 @test (@chain begin
@@ -47,11 +47,11 @@ test_function(a, b; c = 4) = a - b + c
 test_function(a, b) = vcat(a, b)
 
 @test (@chain begin
-                  collect_arguments([1, 2], [3])
+                  collect_arguments([1, 2], [3,4])
                   LazyCall(test_function)
                   run(broadcast)
               end) ==
-      [ [1, 3], [2, 3] ]
+      [ [1, 3], [2, 4] ]
 @test ( @lazy_call +(1, 2) ) ==
       collect_call(+, 1, 2)
 
