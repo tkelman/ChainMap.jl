077701d99d88e8d6a19500a1a2f10d726dbe19d8
Brandon Taylor <brandon.taylor221@gmail.com>
2016-08-12 08:18:47 -0400

    Added back in non-standard

--- src/nonstandard.jl
+++ src/nonstandard.jl
@@ -1,17 +1,34 @@
-export @chain
-macro chain(es...)
-    esc(ChainMap.chain(es...))
+function nonstandard1(f)
+  macro_f = Expr(:quote, Expr(:macrocall, Symbol("@" * string(f))))
+  quote
+      macro $f(args...)
+          esc($f(args...) )
+      end
+      @doc (@doc $f) $macro_f
+  end
 end
-@doc chain :(@chain)
 
-export @unweave
-macro unweave(es...)
-    esc(ChainMap.unweave(es...))
-end
-@doc unweave :(@unweave)
+"""
+   @nonstandard(fs...)
+Will create a nonstandard evaluation macro for each of the fs functions.
 
-export @lazy_call
-macro lazy_call(es...)
-    esc(ChainMap.lazy_call(es...))
-end
-@doc lazy_call :(@lazy_call)
+Each function should be a function that takes and returns expressions. The
+nonstandard macro will have the same name but will take in code, not
+expressions, and will evaluate the result locally when the macro is called. Will
+copy over the docstrings from the standard version to the nonstandard version.
+#Examples
+```julia
+binaryfun(a, b, c) = :(\$b(\$a, \$c))
+chainback(a, b, c) = :(\$c(\$b, \$a))
+@nonstandard binaryfun chainback
+@test (@binaryfun 1 vcat 2) == vcat(1, 2)
+@test (@chainback 2 3 vcat) == vcat(3, 2)
+```
+"""
+nonstandard(fs...) = Expr(:block, map(nonstandard1, fs)...)
+
+export @nonstandard
+eval(nonstandard(:nonstandard))
+
+export @chain, @unweave, @lazy_call
+@nonstandard chain unweave lazy_call
