04374234ecfa3e2be7da8c26244a6066eea0bb14
Brandon Taylor <brandon.taylor221@gmail.com>
2016-10-12 23:13:57 -0400

    added another link method

--- src/chain.jl
+++ src/chain.jl
@@ -1,7 +1,23 @@
-export chain
+export link
 
 """
     @link head tail
+
+`head[tail]`
+
+# Examples
+```julia
+v = [1, 2, 3]
+@test v[2] == @link v 2
+
+d = Dict( a => 1, b => 2)
+@test d[:a] == @link d :a
+```
+"""
+link(head, tail) = Expr(:ref, head, tail)
+
+"""
+    @link head tail::Symbol
 
 Calls `head` on `tail`
 
@@ -10,13 +26,12 @@ Calls `head` on `tail`
 @test vcat(1) == @link 1 vcat
 ```
 """
-link(head, tail) = Expr(:call, tail, head)
+link(head, tail:::Symbol) = Expr(:call, tail, head)
 
 """
     @link head tail::Expr
 
-Reinterprets `\_` in `tail` as `head`. Note that dot vectorization is broken by
-this macro. Instead, use [`unweave`](@ref), [`@map`](@ref), or [`@broadcast`](@ref)
+Reinterprets `\_` in `tail` as `head`.
 
 # Examples
 ```julia
@@ -28,10 +43,14 @@ link(head, tail::Expr) = Expr(:let, tail, Expr(:(=), :_, head))
 link(head, tail::AnnotatedLine) =
     AnnotatedLine(tail.line, link(convert(Expr, head), tail.expr) )
 
+@nonstandard link
+export @link
+
+export chain_line
 """
     @chain_line es...
 
-`reduce` `@chain` over `es`
+`reduce` `@link` over `es`
 
 # Examples
 ```julia
@@ -41,11 +60,16 @@ link(head, tail::AnnotatedLine) =
 """
 chain_line(es...) = foldl(link, es)
 
+@nonstandard chain_line
+export @chain_line
+
 """
     @chain e
 
-Separate single begin blocks out into lines and [`chain`](@ref) them;
-return single non-blocks.
+Separate single begin blocks out into lines and [`chain_line`](@ref) them;
+return single non-blocks. Note that dot fusion is broken by
+this macro. Instead, use [`unweave`](@ref), [`@map`](@ref), or
+[`@broadcast`](@ref).
 
 # Examples
 ```julia
