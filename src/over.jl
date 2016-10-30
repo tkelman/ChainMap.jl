6ee7cba8a32a28bd9d05509f190fbac9388325fe
Brandon Taylor <brandon.taylor221@gmail.com>
2016-10-29 22:47:53 -0400

    back to broadcast

--- src/over.jl
+++ src/over.jl
@@ -1,24 +1,24 @@
 export over
 """
-    @over(e::Expr, f::Symbol = map)
+    @over(e::Expr, f::Symbol = broadcast)
 
 [`unweave`](@ref) `e` then run `f` on the component parts, anonymous function
 first.
 
 # Examples
 ```julia
-e = :(vcat(~a, ~b) )
-f = :map
+e = :( vcat(~a, ~b) )
+f = :broadcast
 over(e, f)
 
 a = [1, 2]
 b = [3, 4]
 
-@test map((a, b) -> vcat(a, b), a, b) ==
+@test broadcast( (a, b) -> vcat(a, b), a, b) ==
     @over vcat(~a, ~b)
 ```
 """
-function over(e::Expr, f::Symbol = :map)
+function over(e::Expr, f::Symbol = :broadcast)
 
     anonymous_function, anonymous_arguments = split_anonymous(e)
 
@@ -36,25 +36,25 @@ the woven arguments at the end of the arguments of `f`.
 
 # Examples
 ```julia
-map_tuple(args...; as_tuple = false) =
+broadcast_tuple(args...; as_tuple = false) =
     if as_tuple
-        (map(args...)...)
+        (broadcast(args...)...)
     else
-        map(args...)
+        broadcast(args...)
     end
 
 e = :( vcat(~a, ~b) )
-f = :(map_tuple(as_tuple = true) )
+f = :(broadcast_tuple(as_tuple = true) )
 
 over(e, f)
 
 a = [1, 2]
 b = [3, 4]
 
-result = @over ~a + ~b map_tuple(as_tuple = true)
+result = @over ~a + ~b broadcast_tuple(as_tuple = true)
 
-@test map_tuple( (a, b) -> vcat(a, b), a, b, as_tuple = true) ==
-    @over vcat(~a, ~b) map_tuple(as_tuple = true)
+@test broadcast_tuple( (a, b) -> vcat(a, b), a, b, as_tuple = true) ==
+    @over vcat(~a, ~b) broadcast_tuple(as_tuple = true)
 
 # `f` must be a call
 @test_throws ErrorException over(:(~_ + 1), :(import ChainMap) )
